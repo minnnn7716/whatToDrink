5a065fcb023f851d920325b2a99c13488e39a177
minnnn7716 <86654762+minnnn7716@users.noreply.github.com>
2022-12-16 16:13:02 +0800

    building

--- JS/shopMenu.js
+++ JS/shopMenu.js
@@ -2,6 +2,7 @@ const url = location.href;
 let keyword = decodeURIComponent(url.split("=")[1].split("&")[0]);
 
 let shopsData;
+let singleShopsData;
 let shopId;
 
 getShops();
@@ -14,6 +15,7 @@ function getShops() {
         .then(function (response) {
             shopsData = response.data;
             getShopId();
+            renderHeader();
         })
         .catch(function (error) {
             console.log(error);
@@ -21,14 +23,51 @@ function getShops() {
 }
 
 function getShopId() {
-    let data = shopsData.filter(item => item.name === keyword);
-    shopId = data[0].id;
+    singleShopsData = shopsData.filter(item => item.name === keyword)[0];
+    shopId = singleShopsData.id;
 }
 
-function renderBanner() {
+function renderHeader() {
+    const headerInfor = document.querySelector("#menuBanner");
+    const headerImg = document.querySelector(".shops-menu-banner ");
 
+    let imgStr = `<img src="${singleShopsData.photoUrl}" alt="${singleShopsData.name}">`
+
+    let inforStr = `
+    <div class="menu-title col-12 flex justify-between">
+    <h2 class="menu-title-text">${singleShopsData.name}</h2>
+    <a href="#" class="menu-title-rate">
+        <div class="rateSection">
+        <p class="score mr-1">4.9</p>
+        <div class="starSection mb-1">
+            <span class="star"></span>
+            <span class="star"></span>
+            <span class="star"></span>
+            <span class="star"></span>
+            <span class="star"></span>
+        </div>
+        </div>
+        <p class="rate-num">1,423 則評論</p>
+    </a>
+    </div>
+    `;
+
+    headerInfor.innerHTML = inforStr;
+    headerImg.innerHTML = imgStr;
 }
 
 function renderMenu() {
 
 }
+
+// {
+//     "id": 3,
+//     "name": "茶湯會",
+//     "size": {
+//       "m": "480ml",
+//       "l": "660ml"
+//     },
+//     "photoUrl": "https://raw.githubusercontent.com/minnnn7716/whatToDrink/c08e625b274802bda3a7a89f74414c812e911f57/IMAGES/logo_%E8%8C%B6%E6%B9%AF%E6%9C%83.svg",
+//     "rate": ,
+//     "rateNum": 
+//   }
