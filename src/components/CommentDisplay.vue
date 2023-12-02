855626bac2beba89cc3ac5133d6f07adda277631
m_m <minnn7716@gmail.com>
2023-12-02 14:11:56 +0800

    feat: 店家評價 Modal

--- src/components/CommentDisplay.vue
+++ src/components/CommentDisplay.vue
@@ -9,25 +9,27 @@ export default {
 </script>
 
 <template>
-  <div class="commentDisplay">
-    <div class="commentDisplay-userPhoto">
-      <img class="img-fluid" src="../assets/images/userPhoto.png" alt="使用者頭像" />
-    </div>
-    <div class="mb-4 d-flex justify-content-between" style="margin-top: -24xp">
-      <div class="bg-white px-2">
-        <h5 class="mb-1">漢堡神偷愛喝飲料</h5>
-        <p class="fs-normal2">18 則評論</p>
+  <div class="pt-6 flex-grow-1">
+    <div class="commentDisplay h-100">
+      <div class="commentDisplay-userPhoto">
+        <img class="img-fluid" src="../assets/images/userPhoto.png" alt="使用者頭像" />
       </div>
-      <div class="bg-white px-2">
-        <RateDisplay class="rateDisplay-sm mb-1" />
-        <p class="fs-normal2 text-end">3 分糖 / 去冰</p>
+      <div class="commentDisplay-header mb-4 d-flex justify-content-between">
+        <div class="bg-white px-2">
+          <h5 class="mb-1">漢堡神偷愛喝飲料</h5>
+          <p class="fs-normal2">18 則評論</p>
+        </div>
+        <div class="bg-white px-2">
+          <RateDisplay class="rateDisplay-sm mb-1" />
+          <p class="fs-normal2 text-end">3 分糖 / 去冰</p>
+        </div>
       </div>
+      <p class="commentDisplay-content px-2">
+        茶與葡萄柚果汁的調配完全恰到好處！<br />
+        根本夏日聖品，清爽解膩，3 分糖去冰剛剛好<br />
+        如果點多冰的話半糖應該滿不錯的～
+      </p>
     </div>
-    <p class="px-2">
-      茶與葡萄柚果汁的調配完全恰到好處！<br />
-      根本夏日聖品，清爽解膩，3 分糖去冰剛剛好<br />
-      如果點多冰的話半糖應該滿不錯的～
-    </p>
   </div>
 </template>
 
@@ -41,13 +43,27 @@ export default {
   &::after {
     content: '';
     position: absolute;
-    z-index: -1;
     top: 12px;
     right: 0;
     width: 100%;
     height: 100%;
     border: 1px solid #000;
     border-radius: 16px;
+  }
+
+  &-header,
+  &-content {
+    position: relative;
+  }
+
+  &-header,
+  &-userPhoto,
+  &-content {
+    z-index: 5;
+  }
+
+  &-header {
+    margin-top: -24px;
   }
 
   &-userPhoto {
--- src/views/front/ShopView.vue
+++ src/views/front/ShopView.vue
@@ -1,11 +1,16 @@
 <script>
+import 'bootstrap/js/src/modal';
 import FavoriteBtn from '../../components/FavoriteBtn.vue';
 import RateDisplay from '../../components/RateDisplay.vue';
+import RateGroup from '../../components/RateGroup.vue';
+import CommentDisplay from '../../components/CommentDisplay.vue';
 
 export default {
   components: {
     RateDisplay,
+    RateGroup,
     FavoriteBtn,
+    CommentDisplay,
   },
 };
 </script>
@@ -21,7 +26,12 @@ export default {
         <div class="row justify-content-center">
           <div class="col-10 col-3xl-12 d-flex justify-content-between">
             <h2 class="fs-3 fw-bold title-stroke-2">清心福全</h2>
-            <button type="button" class="btn border-0 p-0">
+            <button
+              type="button"
+              class="btn border-0 p-0"
+              data-bs-toggle="modal"
+              data-bs-target="#shopRateModal"
+            >
               <RateDisplay class="rateDisplay-md hasHandText strokeTitle mb-2" />
               <p class="text-end">57 則評論</p>
             </button>
@@ -782,6 +792,140 @@ export default {
         </div>
       </div>
     </section>
+
+    <!-- shopRateModal -->
+    <div class="modal fade" id="shopRateModal">
+      <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content rounded-4">
+          <div class="modal-header border-bottom-0">
+            <h1 class="modal-title fs-5 invisible" id="addCommentModalLabel">店家評價</h1>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body pt-0 pb-10">
+            <div class="container-fluid">
+              <RateGroup class="mb-10" />
+              <div class="d-flex align-items-center justify-content-end mb-4">
+                <label for="type" class="me-4 fw-medium">排序</label>
+                <select
+                  id="type"
+                  class="form-select py-2 px-4 border-black rounded-pill"
+                  style="max-width: 180px"
+                >
+                  <option selected value="評分最高">評分最高</option>
+                  <option value="最新日期">最新日期</option>
+                  <option value="最冰溫度">最冰溫度</option>
+                  <option value="最少甜度">最少甜度</option>
+                </select>
+              </div>
+              <div class="px-5">
+                <div class="pb-10 mb-10 border-bottom border-gray">
+                  <div class="row">
+                    <div class="col-5">
+                      <div class="d-flex">
+                        <img
+                          width="170"
+                          height="170"
+                          class="me-3"
+                          src="../../assets/images/drinkPhoto.png"
+                          alt=""
+                        />
+                        <div class="d-flex flex-column justify-content-between">
+                          <div>
+                            <p class="fs-5 fw-medium mb-2">珍珠多多綠</p>
+                            <div class="d-flex align-items-center">
+                              <img
+                                width="20"
+                                class="me-1"
+                                src="../../assets/images/icon-start-full.svg"
+                                alt="rate"
+                              />
+                              <p class="fs-6 font-handwriting fw-bold mb-minus1">4.9</p>
+                            </div>
+                          </div>
+                          <p class="fs-6 font-handwriting lh-lg">
+                            M <span class="ms-1">$ 50</span>
+                            <span class="mx-2">｜</span>
+                            L <span class="ms-1">$ 60</span>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-7 d-flex">
+                      <CommentDisplay />
+                    </div>
+                  </div>
+                </div>
+                <div class="pb-10 mb-10 border-bottom border-gray">
+                  <div class="row">
+                    <div class="col-4 d-flex">
+                      <div class="w-55 me-3">
+                        <img class="img-full" src="../../assets/images/drinkPhoto.png" alt="" />
+                      </div>
+                      <div class="d-flex flex-column justify-content-between">
+                        <div>
+                          <p class="fs-5 fw-medium mb-2">珍珠多多綠</p>
+                          <div class="d-flex align-items-center">
+                            <img
+                              width="20"
+                              class="me-1"
+                              src="../../assets/images/icon-start-full.svg"
+                              alt="rate"
+                            />
+                            <p class="fs-6 font-handwriting fw-bold mb-minus1">4.9</p>
+                          </div>
+                        </div>
+                        <p class="fs-6 font-handwriting lh-lg">
+                          M $50 <br />
+                          L $60
+                        </p>
+                      </div>
+                    </div>
+                    <div class="col-8 d-flex">
+                      <CommentDisplay />
+                    </div>
+                  </div>
+                </div>
+                <div class="pb-10 mb-10 border-bottom border-gray">
+                  <div class="row">
+                    <div class="col-4 d-flex">
+                      <div class="w-55 me-3">
+                        <img class="img-full" src="../../assets/images/drinkPhoto.png" alt="" />
+                      </div>
+                      <div class="d-flex flex-column justify-content-between">
+                        <div>
+                          <p class="fs-5 fw-medium mb-2">珍珠多多綠</p>
+                          <div class="d-flex align-items-center">
+                            <img
+                              width="20"
+                              class="me-1"
+                              src="../../assets/images/icon-start-full.svg"
+                              alt="rate"
+                            />
+                            <p class="fs-6 font-handwriting fw-bold mb-minus1">4.9</p>
+                          </div>
+                        </div>
+                        <p class="fs-6 font-handwriting lh-lg">
+                          M $50 <br />
+                          L $60
+                        </p>
+                      </div>
+                    </div>
+                    <div class="col-8 d-flex">
+                      <CommentDisplay />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
