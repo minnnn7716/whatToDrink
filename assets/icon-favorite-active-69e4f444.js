be72f4a907a147deba69a69973b292693e952c99
m_m <minnn7716@gmail.com>
2023-12-17 15:16:39 +0800

    deploy

---
+++ assets/icon-favorite-active-69e4f444.js
@@ -0,0 +1,1 @@
+const s="/whatToDrink/assets/icon-favorite-a6252f93.svg",a="/whatToDrink/assets/icon-favorite-active-1af73e42.svg";export{s as _,a};
---
+++ assets/userPhoto-8e961c51.js
@@ -0,0 +1,1 @@
+const s="/whatToDrink/assets/userPhoto-03d36aa3.png";export{s as _};
