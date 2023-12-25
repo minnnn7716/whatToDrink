8c02737c7c2d83f6831ef7953644e45c11707a24
m_m <minnn7716@gmail.com>
2023-12-25 13:39:43 +0800

    feat: 404 頁面, meta title

--- src/router/index.js
+++ src/router/index.js
@@ -4,49 +4,88 @@ const routes = [
   {
     path: '/',
     name: 'FrontLayout',
-    component: () => import('../views/FrontLayout.vue'),
+    component: () => import('@/views/FrontLayout.vue'),
     children: [
       {
         path: '/',
         name: 'Home',
-        component: () => import('../views/front/HomeView.vue'),
+        component: () => import('@/views/front/HomeView.vue'),
+        meta: {
+          title: 'What to Drink',
+        },
       },
       {
         path: '/search',
         name: 'Search',
-        component: () => import('../views/front/SearchView.vue'),
+        component: () => import('@/views/front/SearchView.vue'),
+        meta: {
+          title: 'What to Drink - 搜尋',
+        },
       },
       {
         path: '/shops',
         name: 'Shop',
-        component: () => import('../views/front/ShopsList.vue'),
+        component: () => import('@/views/front/ShopsList.vue'),
+        meta: {
+          title: 'What to Drink - 店家列表',
+        },
       },
       {
         path: '/shops/:id',
-        component: () => import('../views/front/ShopView.vue'),
+        component: () => import('@/views/front/ShopView.vue'),
         props: (route) => ({ id: route.params.id }),
+        meta: {
+          title: 'What to Drink - 店家',
+        },
       },
       {
         path: '/drink/:id',
-        component: () => import('../views/front/DrinkView.vue'),
+        component: () => import('@/views/front/DrinkView.vue'),
         props: (route) => ({ id: route.params.id }),
+        meta: {
+          title: 'What to Drink - 飲料',
+        },
       },
       {
         path: '/about',
         name: 'About',
-        component: () => import('../views/front/AboutView.vue'),
+        component: () => import('@/views/front/AboutView.vue'),
+        meta: {
+          title: 'What to Drink - 關於',
+        },
       },
       {
         path: '/favorites',
         name: 'Favorite',
-        component: () => import('../views/front/FavoriteView.vue'),
+        component: () => import('@/views/front/FavoriteView.vue'),
+        meta: {
+          title: 'What to Drink - 喜愛清單',
+        },
       },
       {
         path: '/wheel',
         name: 'SpinWheel',
-        component: () => import('../views/front/SpinWheel.vue'),
+        component: () => import('@/views/front/SpinWheel.vue'),
+        meta: {
+          title: 'What to Drink - 轉盤',
+        },
+      },
+      {
+        path: '/notfound',
+        name: 'NotFound',
+        component: () => import('@/views/NotFound.vue'),
+        meta: {
+          title: 'What to Drink - 找不到頁面',
+        },
       },
     ],
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: {
+      name: 'NotFound',
+      params: {},
+    },
   },
 ];
 
@@ -61,6 +100,13 @@ const router = createRouter({
     }
     return { top: 0 };
   },
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title;
+  }
+  next();
 });
 
 export default router;
---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="d-flex flex-column align-items-center justify-content-center">
+    <h2 class="font-handwriting mb-3 fs-3 fs-md-2" >PAGE NOT FOUND</h2>
+    <p class="fs-6 mb-5 text-center">很抱歉，您查詢的網頁<br class="d-md-none">可能已經移除或變更網址</p>
+    <RouterLink
+      to="/"
+      class="btn-custom btn-custom-primary-sm fs-6 fw-bold"
+    >
+      返回首頁
+    </RouterLink>
+  </div>
+</template>
